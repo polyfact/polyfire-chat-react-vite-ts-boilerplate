32ed43355cbca8eb7d2bcd6a3d3a174657fc0ab6
kgricour <kevin@newton.so>
2023-08-18 20:46:20 +0200

    add few systemPromptId to test

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,9 @@ import config from "./config";
 
 import "./App.css";
 
-const BOT_NAME = "Seraphina";
-
 function App() {
   const { polyfact, login, loading } = usePolyfact({
-    project: "7b29de50-8ce4-4449-8382-115eee716f76",
+    project: "7b29de50-8ce4-4449-8382-115eee716f76", // create your own project at https://app.polyfact.com
   });
   const [chat, setChat] = useState<Chat>();
 
@@ -23,7 +21,12 @@ function App() {
       setChat(
         new polyfact.Chat({
           systemPromptId:
-            config.chat.promptId || "49735ec7-6c20-4ceb-9741-3de1db4fe6cd", // virtual soft girlfriend
+            // Here a few prompt id to test (create your own with the Polyfact sdk or just use the prop systemPrompt)
+            // chucky prompt : cb93e8e4-23fe-4a9d-b2b2-0b910d438509
+            // virtual soft girlfriend prompt : 49735ec7-6c20-4ceb-9741-3de1db4fe6cd
+            // midjourney prompt : f4a1f732-9c38-4bdb-b9e4-3baa7971286a
+            // Holy bible prompt : 8fc39ca4-3941-40d9-824a-5ed283102f6e
+            config.chat.promptId || "cb93e8e4-23fe-4a9d-b2b2-0b910d438509",
           autoMemory: true,
         })
       );
@@ -31,7 +34,10 @@ function App() {
   }, [polyfact]);
 
   return (
-    <div className="app-container">
+    <div
+      className="app-container"
+      style={{ backgroundColor: config.chat.botMessageBackgroundColor }}
+    >
       <Header title={config.chat.botName} {...config.header} />
       <div className="content">
         {login ? (
